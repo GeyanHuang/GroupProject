9fdbee06b13fc1fb22f77fa80fe2b4dfacf6e9f6
cliff <cliff_ok@outlook.com>
2017-11-08 07:34:36 -0500

    Commit By Geyan Huang

--- app/src/main/java/cst2335/groupproject/Home.java
+++ app/src/main/java/cst2335/groupproject/Home.java
@@ -2,9 +2,6 @@ package cst2335.groupproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,7 +13,6 @@ import android.view.MenuItem;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
