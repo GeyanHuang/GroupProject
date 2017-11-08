d45b316a50a1c780515f31b10d88f31bc3bc610b
cliff <cliff_ok@outlook.com>
2017-11-08 13:39:03 -0500

    Commit By Geyan Huang

--- app/src/main/java/cst2335/groupproject/PkgActivity/ActivityFragment.java
+++ app/src/main/java/cst2335/groupproject/PkgActivity/ActivityFragment.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
 import cst2335.groupproject.R;
 
 
@@ -42,19 +43,24 @@ public class ActivityFragment extends Fragment {
 
     private View view;
     private ListView listView;
-    private TextView item;
+    private TextView item, min, date, desc;
     private ImageView image;
     private ArrayList<Info> info;
 
     private class Info {
         private String item;
+        private String min, date, desc;
         private int image;
 
-        public Info(String item, int image) {
+        public Info(String item, String min, String date, String desc, int image) {
             this.item = item;
+            this.min = min;
+            this.date = date;
+            this.desc = desc;
             this.image = image;
         }
-        public String getItem(){
+
+        public String getItem() {
             return item;
         }
     }
@@ -71,7 +77,13 @@ public class ActivityFragment extends Fragment {
             View customView = inflater.inflate(R.layout.activity_info, parent, false);
             item = customView.findViewById(R.id.textview_activity_item);
             image = customView.findViewById(R.id.imageview_activity_item);
+            min = customView.findViewById(R.id.textview_activity_min);
+            date = customView.findViewById(R.id.textview_activity_date);
+            desc = customView.findViewById(R.id.textview_activity_desc);
             item.setText(getItem(position).item);
+            min.setText(getItem(position).min);
+            date.setText(getItem(position).date);
+            desc.setText("("+getItem(position).desc+")");
             image.setImageResource(getItem(position).image);
             return customView;
         }
@@ -83,11 +95,14 @@ public class ActivityFragment extends Fragment {
 
         info = new ArrayList<>();
         String[] items = getResources().getStringArray(R.array.activity_item_list);
+        String[] mins = getResources().getStringArray(R.array.activity_item_min);
+        String[] dates = getResources().getStringArray(R.array.activity_item_date);
+        String[] descs = getResources().getStringArray(R.array.activity_item_desc);
         TypedArray images = getResources().obtainTypedArray(R.array.activity_image_list);
-        for(int i = 0; i < items.length; i++)
-            info.add(new Info(items[i], images.getResourceId(i,-1)));
+        for (int i = 0; i < items.length; i++)
+            info.add(new Info(items[i], mins[i], dates[i], descs[i], images.getResourceId(i, -1)));
         listView = view.findViewById(R.id.listview_activity);
-        InfoAdapter adapter = new InfoAdapter(view.getContext(),info);
+        InfoAdapter adapter = new InfoAdapter(view.getContext(), info);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -95,7 +110,7 @@ public class ActivityFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Info info = (Info) (adapterView.getItemAtPosition(i));
                 String item = info.getItem();
-                Toast.makeText(view.getContext(),item, Toast.LENGTH_SHORT).show();
+                Toast.makeText(view.getContext(), item, Toast.LENGTH_SHORT).show();
             }
         });
 
