b52ef1b3b314dd4fad03a2cf21ddcf9defc74b3e
cliff <cliff_ok@outlook.com>
2017-11-09 09:45:13 -0500

    Commit By Geyan Huang

--- app/src/main/java/cst2335/groupproject/PkgActivity/ActivityInsert.java
+++ app/src/main/java/cst2335/groupproject/PkgActivity/ActivityInsert.java
@@ -2,14 +2,36 @@ package cst2335.groupproject.PkgActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cst2335.groupproject.R;
 
 public class ActivityInsert extends Activity {
+    private Button insert, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        insert = findViewById(R.id.activity_insert_insertbutton);
+        cancel = findViewById((R.id.activity_insert_cancelbutton));
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();;
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();;
+            }
+        });
     }
+
+
 }
