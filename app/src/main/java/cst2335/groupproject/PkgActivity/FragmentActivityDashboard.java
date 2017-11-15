8952c77c52823d6db7729d34f9430c5355cd6205
cliff <cliff_ok@outlook.com>
2017-11-15 10:47:47 -0500

    Commit By Geyan Huang

--- app/src/main/java/cst2335/groupproject/PkgActivity/FragmentActivityDashboard.java
+++ app/src/main/java/cst2335/groupproject/PkgActivity/FragmentActivityDashboard.java
@@ -2,19 +2,25 @@ package cst2335.groupproject.PkgActivity;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import cst2335.groupproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentActivityDashboard extends Fragment {
+public class FragmentActivityDashboard extends Fragment implements View.OnClickListener {
 
+    private View view;
+    private LinearLayout setDailyGoal;
+    private TextView dailyGoal;
 
     public FragmentActivityDashboard() {
         // Required empty public constructor
@@ -25,12 +31,34 @@ public class FragmentActivityDashboard extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity_dashboard, container, false);
+        view = inflater.inflate(R.layout.fragment_activity_dashboard, container, false);
+        setDailyGoal = view.findViewById(R.id.activity_dashboard_setdailygoal);
+        setDailyGoal.setOnClickListener(this);
+        return view;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        dailyGoal = view.findViewById(R.id.activity_dashboard_dailygoal);
+
+    }
+
 
     @Override
     public void onResume() {
         super.onResume();
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.activity_nav_bot_dashboard);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.activity_dashboard_setdailygoal:
+
+                break;
+        }
+    }
 }
+
