633df3f39918ee60686f3feacc0e1c6f168fdb8c
cliff <cliff_ok@outlook.com>
2017-11-14 23:59:00 -0500

    Commit By Geyan Huang

---
+++ app/src/main/java/cst2335/groupproject/ActivityNavBot.java
@@ -0,0 +1,13 @@
+package cst2335.groupproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityNavBot extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_bot);
+    }
+}
