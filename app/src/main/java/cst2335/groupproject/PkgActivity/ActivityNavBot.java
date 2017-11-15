2600128e99ec168bdc6378463baf1a3d053cf47a
cliff <cliff_ok@outlook.com>
2017-11-14 23:59:23 -0500

    Commit By Geyan Huang

--- app/src/main/java/cst2335/groupproject/PkgActivity/ActivityNavBot.java
+++ app/src/main/java/cst2335/groupproject/PkgActivity/ActivityNavBot.java
@@ -1,7 +1,9 @@
-package cst2335.groupproject;
+package cst2335.groupproject.PkgActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import cst2335.groupproject.R;
 
 public class ActivityNavBot extends Activity {
 
