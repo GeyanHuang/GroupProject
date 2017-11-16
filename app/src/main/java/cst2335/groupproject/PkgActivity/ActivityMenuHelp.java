566b9265232d30e2926861f4f1f351d1585ce698
cliff <cliff_ok@outlook.com>
2017-11-16 15:46:38 -0500

    Commit By Geyan Huang

--- app/src/main/java/cst2335/groupproject/PkgActivity/ActivityMenuHelp.java
+++ app/src/main/java/cst2335/groupproject/PkgActivity/ActivityMenuHelp.java
@@ -1,22 +1,18 @@
 package cst2335.groupproject.PkgActivity;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.util.Locale;
 
 import cst2335.groupproject.R;
 
@@ -50,7 +46,6 @@ public class ActivityMenuHelp extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
 
 
     }
@@ -88,8 +83,32 @@ public class ActivityMenuHelp extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_activity_menu_help, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            ImageView imageView = (ImageView) rootView.findViewById(R.id.section_label);
+
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+                case 1:
+                    if (Locale.getDefault().getLanguage().equals("zh")) {
+                        imageView.setImageResource(R.drawable.activity_help_p1_zh);
+                    }else {
+                        imageView.setImageResource(R.drawable.activity_help_p1);
+                    }
+                    break;
+                case 2:
+                    if (Locale.getDefault().getLanguage().equals("zh")) {
+                        imageView.setImageResource(R.drawable.activity_help_p2_zh);
+                    }else{
+                        imageView.setImageResource(R.drawable.activity_help_p2);
+                    }
+                    break;
+                case 3:
+                    if (Locale.getDefault().getLanguage().equals("zh")) {
+                        imageView.setImageResource(R.drawable.activity_help_p3_zh);
+                    }else{
+                        imageView.setImageResource(R.drawable.activity_help_p3);
+                    }
+                    break;
+            }
+
             return rootView;
         }
     }
