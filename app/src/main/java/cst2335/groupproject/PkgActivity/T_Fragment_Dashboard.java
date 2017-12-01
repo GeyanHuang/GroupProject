31185833c878f9189cbadea3a34342a673d05af7
cliff <cliff_ok@outlook.com>
2017-11-30 20:59:02 -0500

    Commit By Geyan Huang

--- app/src/main/java/cst2335/groupproject/PkgActivity/T_Fragment_Dashboard.java
+++ app/src/main/java/cst2335/groupproject/PkgActivity/T_Fragment_Dashboard.java
@@ -6,7 +6,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
