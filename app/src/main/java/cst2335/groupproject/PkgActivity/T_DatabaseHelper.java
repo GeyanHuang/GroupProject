07efa070d44ad8fed447ef0f5a6a49e98fd3ddac
cliff <cliff_ok@outlook.com>
2017-12-02 17:17:27 -0500

    Commit By Geyan Huang

--- app/src/main/java/cst2335/groupproject/PkgActivity/T_DatabaseHelper.java
+++ app/src/main/java/cst2335/groupproject/PkgActivity/T_DatabaseHelper.java
@@ -13,20 +13,60 @@ import android.database.sqlite.SQLiteOpenHelper;
  *
  */
 public class T_DatabaseHelper extends SQLiteOpenHelper {
+
+    /**
+     * Database name
+     */
     private static final String DATABASE_NAME = "Activity.db";
+
+    /**
+     * Database version
+     */
     private static final int DATABASE_VERSION = 1;
 
+    /**
+     * Table Name
+     */
     public static final String TABLE_NAME = "Activity";
+
+    /**
+     * Column ID
+     */
     public static final String COLUMN_ID = "ActivityID";
+
+    /**
+     * Column minute
+     */
     public static final String COLUMN_MINUTE = "Minute";
+
+    /**
+     * Column type
+     */
     public static final String COLUMN_TYPE = "Type";
+
+    /**
+     * Column date
+     */
     public static final String COLUMN_DATE = "Date";
+
+    /**
+     * Column time
+     */
     public static final String COLUMN_TIME = "Time";
+
+    /**
+     * Column comment
+     */
     public static final String COLUMN_COMMENT = "Comment";
 
-
+    /**
+     * Database
+     */
     public SQLiteDatabase database;
 
+    /**
+     * Query for create database
+     */
     private static final String TABLE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
             COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             COLUMN_MINUTE + " INTEGER, " +
@@ -36,32 +76,60 @@ public class T_DatabaseHelper extends SQLiteOpenHelper {
             COLUMN_COMMENT + " TEXT" +
             ")";
 
+    /**
+     * Constructor of database helper
+     * @param context The context of current activity
+     */
     public T_DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * Function for creating database
+     * @param sqLiteDatabase The database
+     */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(TABLE_CREATE);
     }
 
+    /**
+     * Function when upgrade version of database, it will drop the table and recreate it (Clear the date)
+     * @param sqLiteDatabase The database
+     * @param i The old version number
+     * @param i1 The new version number
+     */
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 
+    /**
+     * Function for opening database
+     */
     public void openDatabase() {
         database = getWritableDatabase();
     }
 
+    /**
+     * Function for close database
+     */
     public void closeDatabase() {
         if (database != null && database.isOpen()) {
             database.close();
         }
     }
 
-    public long insert(String minute, String type, String date, String time, String comment) {
+    /**
+     * Function for inserting to database
+     * @param minute The activity minute
+     * @param type  The activity type
+     * @param date The date
+     * @param time The time
+     * @param comment The comment
+     */
+    public void insert(String minute, String type, String date, String time, String comment) {
         ContentValues values = new ContentValues();
         values.put(COLUMN_MINUTE, Integer.parseInt(minute));
         values.put(COLUMN_TYPE, type);
@@ -69,16 +137,28 @@ public class T_DatabaseHelper extends SQLiteOpenHelper {
         values.put(COLUMN_TIME, time);
         values.put(COLUMN_COMMENT, comment);
 
-
-        return database.insert(TABLE_NAME, null, values);
+        database.insert(TABLE_NAME, null, values);
 
     }
 
+    /**
+     * Function for deleting a row by ID
+     * @param id The ID
+     */
     public void delete(String id) {
         getWritableDatabase().execSQL("DELETE FROM " + TABLE_NAME + " WHERE " + COLUMN_ID + " = " + id);
     }
 
-    public long update(String id, String minute, String type, String date, String time, String comment) {
+    /**
+     * Function for updating database
+     * @param id The id
+     * @param minute The activity minute
+     * @param type The activity type
+     * @param date The date
+     * @param time The time
+     * @param comment The comment
+     */
+    public void update(String id, String minute, String type, String date, String time, String comment) {
         ContentValues values = new ContentValues();
         values.put(COLUMN_MINUTE, Integer.parseInt(minute));
         values.put(COLUMN_TYPE, type);
@@ -86,10 +166,13 @@ public class T_DatabaseHelper extends SQLiteOpenHelper {
         values.put(COLUMN_TIME, time);
         values.put(COLUMN_COMMENT, comment);
 
-
-        return database.update(TABLE_NAME, values, COLUMN_ID + " = " + id, null);
+        database.update(TABLE_NAME, values, COLUMN_ID + " = " + id, null);
     }
 
+    /**
+     * Function for read data from database
+     * @return The data for the table
+     */
     public Cursor read() {
         return database.query(TABLE_NAME, null, null, null, null, null, null);
     }
