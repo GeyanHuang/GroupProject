77f1162a3efdaeba9edf53ab91b578c213cc6b28
cliff <liu00399@algonquinlive.com>
2017-12-07 17:57:43 -0500

    Geyan commit

--- app/src/main/java/cst2335/groupproject/PkgAutomobile/A_Main.java
+++ app/src/main/java/cst2335/groupproject/PkgAutomobile/A_Main.java
@@ -1,8 +1,5 @@
 package cst2335.groupproject.PkgAutomobile;
 
-
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
